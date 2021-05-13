ad098f61138105aab7abdad174fb7478e39d29e1
Laura <lauraporto596@gmail.com>
2021-05-13 17:33:50 +0200

    components article and register, routes in app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+//Rutas
+import Home from './views/Home/home';
+import Login from './views/Login/login';
+import Profile from './views/Profile/profile';
+import Store from './views/Store/store';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/login' exact component={Login}/>
+          <Route path='/profile' exact component={Profile}/>
+          <Route path='/store' exact component={Store}/>
+        </Switch>      
+      </BrowserRouter>
     </div>
   );
 }
