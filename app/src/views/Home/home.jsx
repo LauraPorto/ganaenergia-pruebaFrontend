4afb7a08ac8777ec9cdcd45e866f1eb0588d838a
Laura <lauraporto596@gmail.com>
2021-05-13 19:19:29 +0200

    implementing login

--- app/src/views/Home/home.jsx
+++ app/src/views/Home/home.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import user from '../../assets/img/user.png';
+import product from '../../assets/img/product.png';
 
 const Home = () => {
+
+    const history = useHistory();
+
     return (
         <div className='home-main-conainer'>
-            
+            <div className="home-icon-menu" onClick={() => history.push('/profile')}>
+                <img src={user} style={{width: '12em', height: '12em', margin: '2em'}}></img>
+            </div>
+            <div className="home-icon-menu" onClick={() => history.push('/store')}>
+                <img src={product} style={{width: '12em', height: '12em', margin: '2em'}}></img>
+            </div>
         </div>
     )
-}
+};
 
 export default Home;
--- app/src/views/Login/login.jsx
+++ app/src/views/Login/login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
+
+    const history = useHistory();
+    
+    //Hooks
+    const [dataLogin, setDataLogin] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleState = (event) => {
+        let data = { ...dataLogin, [event.target.name]: event.target.value };
+        setDataLogin(data);
+    };
+
+    const checkData = async () => {
+        const result = await axios.post('http://localhost:3002/user/login');
+        console.log(result, 'resultado de login');
+    };
+
     return (
-        <div>
-            
+        <div className='login-main-conainer'>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={() => handleState()}/>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" onChange={() => handleState()}/>
+                </div>
+                <button type='submit' onClick={() => checkData()} className='button-login'></button>
+            </form>
         </div>
     )
 }
